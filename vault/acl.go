9158e9344802640661f9237cee5bd368beb18428
Sarah Thompson <sthompson@hashicorp.com>
2020-08-19 13:43:40 +0100

    Update acl.go

--- vault/acl.go
+++ vault/acl.go
@@ -15,7 +15,7 @@ import (
 	"github.com/hashicorp/vault/sdk/helper/strutil"
 	"github.com/hashicorp/vault/sdk/logical"
 	"github.com/mitchellh/copystructure"
-	"github.com/hashicorp/vault-plugin-mock"
+	_ "github.com/hashicorp/vault-plugin-mock"
 )
 
 // ACL is used to wrap a set of policies to provide
